22d4eaa241651afc3555d4e8567dede7d1377ce7
swapnadeepcom <swapnadeeproy672@gmail.com>
2024-09-24 13:04:37 +0530

    Add files via upload

---
+++ library/Authentication.py
@@ -0,0 +1,33 @@
+# Handles Login and Registration
+
+class Authentication:
+    def __init__(self, admins, librarians, users):
+        self.admins = admins
+        self.librarians = librarians
+        self.users = users
+
+    def register(self, role):
+        username = input("Enter username: ")
+        password = input("Enter password: ")
+
+        if role == 'admin':
+            self.admins[username] = password
+        elif role == 'librarian':
+            self.librarians[username] = password
+        elif role == 'user':
+            self.users[username] = password
+
+        print(f"Registration successful for {role}!")
+
+    def login(self, role):
+        username = input("Enter username: ")
+        password = input("Enter password: ")
+
+        if role == 'admin' and self.admins.get(username) == password:
+            print("Logged in as Admin")
+        elif role == 'librarian' and self.librarians.get(username) == password:
+            print("Logged in as Librarian")
+        elif role == 'user' and self.users.get(username) == password:
+            print("Logged in as User")
+        else:
+            print("Invalid credentials")
