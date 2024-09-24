124ad6c0422de31763f8f44b49bd4d6c6435034a
swapnadeepcom <swapnadeeproy672@gmail.com>
2024-09-24 14:17:34 +0530

    Add files via upload

---
+++ library/src/transaction_management.py
@@ -0,0 +1,19 @@
+
+class TransactionManagement:
+    def __init__(self, books, issued_books):
+        self.books = books
+        self.issued_books = issued_books
+
+    def issue_book(self, book_id, user):
+        if book_id in self.books and book_id not in self.issued_books:
+            self.issued_books[book_id] = user
+            print(f"Book '{self.books[book_id]}' issued to {user}.")
+        else:
+            print("Book is either not available or already issued.")
+
+    def return_book(self, book_id):
+        if book_id in self.issued_books:
+            user = self.issued_books.pop(book_id)
+            print(f"Book '{self.books[book_id]}' returned by {user}.")
+        else:
+            print("Book not issued or invalid book ID.")
